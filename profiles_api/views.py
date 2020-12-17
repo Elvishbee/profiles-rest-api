692a848693fbf5c38657b39f786caa5cf71ba41e
Elvishbee <*Betul>
2020-12-17 20:35:52 +0300

    Added POST, PUT, PATCH and DELETE methods to our API View

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,9 +1,12 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework import status #a list of handy http status codes, will be used in our post method
 
+from profiles_api import serializers
 
 class HelloApiView(APIView):
     """Test API View"""
+    serializer_class = serializers.HelloSerializer #whenever your sending a post, put or patch request, expect an input, itll be validated to a max length of 10
 
     def get(self, request, format=None):
         """Returns a list of APIView features"""
@@ -15,3 +18,36 @@ class HelloApiView(APIView):
         ]
 
         return Response({'message': 'Hello!', 'an_apiview': an_apiview})
+
+
+    def post(self, request):
+        """Create a hello message with our name"""
+        serializer = self.serializer_class(data=request.data)
+
+        if serializer.is_valid():
+            name = serializer.validated_data.get('name')
+            message = f'Hello {name}'
+            return Response({'message':message})
+
+        else:
+            return Response(
+                serializer.errors,
+                status=status.HTTP_400_BAD_REQUEST
+            )
+
+
+    def put(self, request, pk=None): #pk> URL primary key
+        """Handle updating an object, completely updates with the new data"""
+        return Response({'method':'PUT'})
+
+
+
+    def patch(self, request, pk=None):
+        """Handle a partial update of an object, only updating some fields"""
+        return Response({'method':'PATCH'})
+
+
+
+    def delete(self, request, pk=None):
+        """Delete an object"""
+        return Response({'method':'DELETE'})
